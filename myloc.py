b7dc4a5abc3d23481aa5bb082eba9ef359e23998
Simranjeet Dua <simranjeet.dua.12@gmail.com>
2018-04-11 21:05:44 +0530

    Added more features and layers

--- myloc.py
+++ myloc.py
@@ -1,10 +1,28 @@
 import folium
+import pandas
 
-map=folium.Map(location=[28.6656074,77.1197519],zoom_start=6,tiles="Mapbox Bright") #created a map object
+map=folium.Map(location=[40.7128,-74.0060],zoom_start=6,tiles="Stamen Terrain") #created a map object
 
 fg=folium.FeatureGroup(name="My Map") #FeatureGroup to turn the layer on and off easily
-for coordinates in [28.6656074,77.1197519],[29.6656074,76.1197519]: #forloop to add multiple markers
-    fg.add_child(folium.Marker(location=coordinates,popup="My Home",icon=folium.Icon(color='blue'))) #added a marker
+
+data=pandas.read_csv("Volcanoes_USA.csv") #converted the csv file of volacanoes to a panda dataframe
+
+latitude=list(data["LAT"]) #made a series(list) of dataframe of latitudes
+longitude=list(data["LON"]) #made a series (list ) of dataframe of longitudes
+elevation=list(data["ELEV"])
+
+def color_producer(el): #used this function to give different marker color according to elevation
+    if el in range(0,2000):
+        return "green"
+    elif el in range(2000,3000):
+        return "orange"
+    else:
+        return "red"
+
+
+for lat,lon,el in zip(latitude,longitude,elevation): #forloop to add multiple markers using zip function
+
+    fg.add_child(folium.CircleMarker(location=[lat,lon],radius=6,popup=str(el)+" metres",fill_color=color_producer(el),color='black',fill=True,fill_opacity=1)) #added a marker
 
 
 
